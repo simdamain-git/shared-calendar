8d333fe6cd57ff8108eca526698bcb4e96922c38
simdamain-git <simdamain@gmail.com>
2024-12-12 21:42:09 +0100

    problème d'icone réglé

--- apps/shared-calendar/frontend/src/app/app.component.ts
+++ apps/shared-calendar/frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IconLoader } from './utils/icon-loader';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
+  constructor() {
+    IconLoader.load();
+  }
 }
--- apps/shared-calendar/frontend/src/app/app.config.ts
+++ apps/shared-calendar/frontend/src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
 import { authInterceptor } from './interceptor/auth.interceptor';
+import { provideIonicAngular } from '@ionic/angular/standalone';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor]), withFetch()), 
+    provideIonicAngular()
     ]
 };
