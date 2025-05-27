965f76a91697e2d8908fb54b3fcce3e5548ec784
simdamain-git <simdamain@gmail.com>
2025-05-27 21:13:56 +0200

    update tsconfig and gitignore

--- apps/shared-calendar/frontend/src/app/services/auth.service.ts
+++ apps/shared-calendar/frontend/src/app/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment.development';
 import { Observable, tap } from 'rxjs';
 import { Router } from '@angular/router';
+import { environment } from '@environments/environment';
 
 interface AuthResponse {
   token: string;
